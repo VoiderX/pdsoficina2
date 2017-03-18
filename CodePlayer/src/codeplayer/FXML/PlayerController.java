c5a50f17bf7dd4d2e5c52609078d551a5e198463
VoiderX <gabrieldeabreu.ti@gmail.com>
2017-03-17 21:18:11 -0300

    Exibindo informações da música ao reproduzir

--- CodePlayer/src/codeplayer/FXML/PlayerController.java
+++ CodePlayer/src/codeplayer/FXML/PlayerController.java
@@ -5,12 +5,16 @@
  */
 package codeplayer.FXML;
 
+import codeplayer.Mp3Buf;
 import codeplayer.Mp3Play;
 import codeplayer.Mp3Stop;
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.Mp3File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -20,6 +24,16 @@ import javafx.fxml.Initializable;
 public class PlayerController implements Initializable {
     Thread mpplay;
     boolean Pausado=false;
+    @FXML
+    Text Faixa;
+    @FXML
+    Text Artista;
+    @FXML
+    Text Titulo;
+    @FXML
+    Text Album;
+    @FXML
+    Text Ano;
      @FXML
      public void play(){
         if(Pausado==false){
@@ -31,6 +45,23 @@ public class PlayerController implements Initializable {
             mpplay.resume();
             Pausado=false;
         }
+        
+        //Codigo para carregar as informações da musica
+        try{
+            Mp3File mp3file = new Mp3File(Mp3Buf.getInstance().getPathMusic());
+            if (mp3file.hasId3v1Tag()) {
+              ID3v1 id3v1Tag = mp3file.getId3v1Tag();
+              Faixa.setText(id3v1Tag.getTrack());
+              Artista.setText(id3v1Tag.getArtist());
+              Titulo.setText(id3v1Tag.getTitle());
+              Album.setText(id3v1Tag.getAlbum());
+              Ano.setText(id3v1Tag.getYear());
+}      
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        //Fim do código para carregar informações da música
      }
      @FXML
      public void stop(){
