15804664a78e11b04851e38dff84b542e827e605
Vinícius Noda <viniciusnoda@alunos.utfpr.edu.br>
2017-05-08 20:58:01 -0300

    labels espectrometro

--- CodePlayer/src/codeplayer/visualizations/SpectrumController.java
+++ CodePlayer/src/codeplayer/visualizations/SpectrumController.java
@@ -28,24 +28,55 @@ public class SpectrumController implements Initializable {
     @FXML
     Pane pane;
     
-    private final int spaceX = 50,spaceY = 50,bands = 80,strokeW = 2,tresh=-80,treshfx=-tresh;
+    private final int spaceX1 = 50,spaceX2=30,spaceY1 = 10,spaceY2=25,bands = 60,strokeW = 2,tresh=-100;
+    private final int maxf=25600;
     private final double inter=0.02;
     /**
      * Initializes the controller class.
      */
     
     public void staticElements(GraphicsContext gc){
+        //background
+        gc.setFill(Color.web("201D1D"));
+        
+        gc.fillRect(0, 0, spec.getWidth(), spec.getHeight());
         //linha vertical
-        gc.strokeLine(spaceX, 0, spaceX, (spec.getHeight()-spaceY));
+        gc.strokeLine(spaceX1, spaceY1, spaceX1, (spec.getHeight()-spaceY2));
         //linha horizontal
-        gc.strokeLine(spaceX,(spec.getHeight()-spaceY),(spec.getWidth()),(spec.getHeight()-spaceY));
+        gc.strokeLine(spaceX1,(spec.getHeight()-spaceY2),(spec.getWidth()-spaceX2),(spec.getHeight()-spaceY2));
+        //labels do y
+        int interv = 5;
+        float text=-tresh;
+        int passo=(-tresh)/interv,displaceY=spaceY1+5;
+        gc.setLineWidth(1);
+        for(int i=0;i<(interv+1);i++){
+            gc.strokeText(""+text,11,displaceY+5);
+            gc.strokeLine(45, displaceY, 50, displaceY);
+            displaceY+=(spec.getHeight()-spaceY1-spaceY2-strokeW)/interv;
+            text-=passo;
+        }
+        //labels do x
+        interv=(bands<=10)?bands:10;
+        text=0;
+        passo=maxf/interv;
+        int displaceX=spaceX1;
+        for(int i=0;i<(interv+1);i++){
+            if(text<1000){
+                gc.strokeText(""+text,displaceX,(spec.getHeight()-spaceY2)+15);
+            }else{
+                gc.strokeText((text/1000)+"k",displaceX,(spec.getHeight()-spaceY2)+15);
+            }
+            gc.strokeLine(displaceX, (spec.getHeight()-spaceY2),displaceX, (spec.getHeight()-spaceY2+5));
+            displaceX+=(spec.getWidth()-spaceX1-spaceX2-strokeW)/interv;
+            text+=passo;
+        }
+        gc.setLineWidth(strokeW);
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         GraphicsContext gc = spec.getGraphicsContext2D();
-        gc.setFill(Color.ROYALBLUE);
-        gc.setStroke(Color.BLACK);
+        gc.setStroke(Color.WHITE);
         gc.setLineWidth(strokeW);
         pane.widthProperty().addListener(event -> teste());
         pane.heightProperty().addListener(event -> teste());
@@ -58,7 +89,7 @@ public class SpectrumController implements Initializable {
     }
     
     public double prop(float mag){
-        return ((mag/(treshfx))*(spec.getHeight()-spaceY));
+        return ((mag/(-tresh))*(spec.getHeight()-spaceY2-spaceY1));
     }
     
     private void start(GraphicsContext gc){
@@ -71,12 +102,13 @@ public class SpectrumController implements Initializable {
             Mp3Buf.getInstance().getMp().setAudioSpectrumListener(new AudioSpectrumListener() {
                 @Override
                 public void spectrumDataUpdate(double d, double d1, float[] mag, float[] phase) {
-                    int displaceX=spaceX+strokeW;
+                    int displaceX=spaceX1+strokeW;
                     gc.clearRect(0, 0, spec.getWidth(), spec.getHeight());
                     staticElements(gc);
+                    gc.setFill(Color.ROYALBLUE);
                     for(int i=0;i<bands;i++){
-                        gc.fillRect((displaceX),((spec.getHeight()-strokeW)-(prop(mag[i]+treshfx))),(spec.getWidth()/bands),(prop(mag[i]+treshfx)-spaceY));
-                        displaceX+=(spec.getWidth()-spaceX-strokeW)/bands;
+                        gc.fillRect((displaceX),((spec.getHeight()-strokeW)-(prop(mag[i]-tresh))),(spec.getWidth()/bands),(prop(mag[i]-tresh)-spaceY2));
+                        displaceX+=(spec.getWidth()-spaceX1-strokeW-spaceX2)/bands;
                         
                     }
                 }
