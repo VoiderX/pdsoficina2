f22ecd566b6d80f00b7cd6bfe99a209e6a276f34
Vinícius Noda <viniciusnoda@alunos.utfpr.edu.br>
2017-05-08 21:30:54 -0300

    Melhorando a IHC do code
    isso não faz sentido

--- CodePlayer/src/codeplayer/visualizations/SpectrumController.java
+++ CodePlayer/src/codeplayer/visualizations/SpectrumController.java
@@ -28,16 +28,21 @@ public class SpectrumController implements Initializable {
     @FXML
     Pane pane;
     
-    private final int spaceX1 = 50,spaceX2=30,spaceY1 = 10,spaceY2=25,bands = 60,strokeW = 2,tresh=-100;
+    //variáveis para o funcionamento(NÃO MEXER)
+    private final int spaceX1 = 50,spaceX2=30,spaceY1 = 10,spaceY2=25,strokeW = 2;
     private final int maxf=25600;
-    private final double inter=0.02;
+    
+    //variáveis para a visualização
+    private int bands = 60,tresh=-100;
+    private double inter=0.02;
+    private String backgroundC ="201D1D",labelC="FFFFFF",blockC="4169E1";
     /**
      * Initializes the controller class.
      */
     
     public void staticElements(GraphicsContext gc){
         //background
-        gc.setFill(Color.web("201D1D"));
+        gc.setFill(Color.web(backgroundC));
         
         gc.fillRect(0, 0, spec.getWidth(), spec.getHeight());
         //linha vertical
@@ -46,14 +51,14 @@ public class SpectrumController implements Initializable {
         gc.strokeLine(spaceX1,(spec.getHeight()-spaceY2),(spec.getWidth()-spaceX2),(spec.getHeight()-spaceY2));
         //labels do y
         int interv = 5;
-        float text=-tresh;
-        int passo=(-tresh)/interv,displaceY=spaceY1+5;
+        float text=0;
+        int passo=(tresh)/interv,displaceY=spaceY1+5;
         gc.setLineWidth(1);
         for(int i=0;i<(interv+1);i++){
             gc.strokeText(""+text,11,displaceY+5);
             gc.strokeLine(45, displaceY, 50, displaceY);
             displaceY+=(spec.getHeight()-spaceY1-spaceY2-strokeW)/interv;
-            text-=passo;
+            text+=passo;
         }
         //labels do x
         interv=(bands<=10)?bands:10;
@@ -76,7 +81,7 @@ public class SpectrumController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         GraphicsContext gc = spec.getGraphicsContext2D();
-        gc.setStroke(Color.WHITE);
+        gc.setStroke(Color.web(labelC));
         gc.setLineWidth(strokeW);
         pane.widthProperty().addListener(event -> teste());
         pane.heightProperty().addListener(event -> teste());
@@ -88,13 +93,12 @@ public class SpectrumController implements Initializable {
         System.out.println("height: "+pane.getHeight());
     }
     
-    public double prop(float mag){
+    public double prop(double mag){
         return ((mag/(-tresh))*(spec.getHeight()-spaceY2-spaceY1));
     }
     
     private void start(GraphicsContext gc){
         staticElements(gc);
-        
         if(Mp3Buf.getInstance().checkmp()!=null){
             Mp3Buf.getInstance().getMp().setAudioSpectrumThreshold(tresh);
             Mp3Buf.getInstance().getMp().setAudioSpectrumNumBands(bands);
@@ -102,12 +106,13 @@ public class SpectrumController implements Initializable {
             Mp3Buf.getInstance().getMp().setAudioSpectrumListener(new AudioSpectrumListener() {
                 @Override
                 public void spectrumDataUpdate(double d, double d1, float[] mag, float[] phase) {
+                    
                     int displaceX=spaceX1+strokeW;
                     gc.clearRect(0, 0, spec.getWidth(), spec.getHeight());
                     staticElements(gc);
-                    gc.setFill(Color.ROYALBLUE);
+                    gc.setFill(Color.web(blockC));
                     for(int i=0;i<bands;i++){
-                        gc.fillRect((displaceX),((spec.getHeight()-strokeW)-(prop(mag[i]-tresh))),(spec.getWidth()/bands),(prop(mag[i]-tresh)-spaceY2));
+                        gc.fillRect((displaceX),((spec.getHeight()-strokeW)-prop((mag[i]-tresh))),(spec.getWidth()/bands),(prop((mag[i]-tresh))-spaceY2));
                         displaceX+=(spec.getWidth()-spaceX1-strokeW-spaceX2)/bands;
                         
                     }
