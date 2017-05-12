9b274fce6a6bdd053f72d51f5a002ea92525ee90
VoiderX <gabrieldeabreu.ti@gmail.com>
2017-05-12 12:34:28 -0300

    Refatoração da Mp3Buf

--- CodePlayer/src/codeplayer/Mp3Buf.java
+++ CodePlayer/src/codeplayer/Mp3Buf.java
@@ -29,70 +29,15 @@ public final class Mp3Buf {
     }
 
     ArrayList<Musica> Musicas; //ArrayList para guardar os caminhos das musicas a serem reproduzidas
-
-    public ArrayList<Musica> getMusicas() {
-        return Musicas;
-    }
-
-    public void setMusicas(ArrayList<Musica> Musicas) {
-        this.Musicas = Musicas;
-    }
-
     String pathMusic;//Caminho da música a ser reproduzida no momento
-
-    public String getPathMusic() {
-        return pathMusic;
-    }
-
-    public void setPathMusic(String pathMusic) {
-        this.pathMusic = pathMusic;
-    }
-
     Media media = null;//Classe Media uitlizada para iniciar o MediaPlayer e também para puxar os metadados
-
-    public Media getMedia() {
-        return media;
-    }
-
-    public void setMedia(Media media) {
-        this.media = media;
-    }
-
     MediaPlayer mp = null; //MediaPlayer, classe responsável pela reprodução em si
-
-    public MediaPlayer checkmp() {
-        return mp;
-    }
-
-    public MediaPlayer getMp() {
-        if (mp == null) {
-            carregaMusica();
-        }
-        return mp;
-    }
-
-    public void setMp(MediaPlayer mp) {
-        this.mp = mp;
-    }
-
     int PosTocando;//Indica a posição que está sendo reproduzida na playlist
-
-    public int getPosTocando() {
-        return PosTocando;
-    }
-
-    public void setPosTocando(int PosTocando) {
-        this.PosTocando = PosTocando;
-    }
     ArrayList<Banda> bandas = null; //Array Contendo os valores das bandas
+    private double lastseekValue; //Ultimo Valor selecionado no tracker
+    private double valorTrackerAnterior; //Valor no tracker antes da seleção
 
-    public ArrayList<Banda> getBandas() {
-        return bandas;
-    }
-
-    public void setBandas(ArrayList<Banda> bandas) {
-        this.bandas = bandas;
-    }
+    //Classe para converter double em horário  
     StringConverter<Double> conversorSliderLabel = new StringConverter<Double>() {
         @Override
         public String toString(Double object) {
@@ -113,27 +58,78 @@ public final class Mp3Buf {
         }
     };
 
+    //Prepara o slider do Seeker
     public void setSeekerSlider() {
+        //Valor maximo é a duração da música
         ControleUI.getInstance().getPlayerController().getTracker().setMax(
                 mp.getMedia().getDuration().toMinutes());
+        //Valor minimo é zero
         ControleUI.getInstance().getPlayerController().getTracker().setMin(0);
-        setValorTrackerAnterior(0);
+        setValorTrackerAnterior(0); //Limpa as váriaveis de controle
         setLastseekValue(0);
+        //Reseta o tracker
         ControleUI.getInstance().getPlayerController().getTracker().setValue(0);
+        //Formata e mostra os labels 
         ControleUI.getInstance().getPlayerController().getTracker().setLabelFormatter(conversorSliderLabel);
         ControleUI.getInstance().getPlayerController().getTracker().setShowTickLabels(true);
     }
+    //Classe listener para a posição atual da música
     InvalidationListener teste = (Observable observable) -> {
-        if (getLastseekValue()>=getValorTrackerAnterior()) {
-            if(mp.getCurrentTime().toMinutes()>=getLastseekValue()){
+        /*Identifica se o usuário retrocedeu ou avançou, e aguarda a atualização do seeker para não
+        Retornar o slider ao valor anterior*/
+        if (getLastseekValue() >= getValorTrackerAnterior()) {
+            if (mp.getCurrentTime().toMinutes() >= getLastseekValue()) {
                 setTrackerSliderPos();
             }
-        }else{
-            if(mp.getCurrentTime().toMinutes()<=getValorTrackerAnterior()){
+        } else {
+            if (mp.getCurrentTime().toMinutes() <= getValorTrackerAnterior()) {
                 setTrackerSliderPos();
             }
-        } 
+        }
     };
+
+    //Metodo para preparar o equalizador
+    private void preparaEq() {
+        mp.getAudioEqualizer().getBands().clear(); //Limpa as bandas do equalizador
+        double freq = 20;
+        for (int i = 0; i <= 9; i++) {
+            Mp3Buf.getInstance().getMp().getAudioEqualizer().getBands().add(new EqualizerBand(freq, freq / 2, 0));
+            freq *= 2;
+        }
+        if (bandas != null) { //Verifica se o array já foi inicializado
+            for (int i = 0; i < bandas.size(); i++) {// Carrega os dados no array
+                mp.getAudioEqualizer().getBands().get(i).setGain(bandas.get(i).getValor());
+            }
+        }
+    }
+
+    //metodo para preparar a media
+    private void preparaMp() {
+        mp = null;
+        media = null;
+        System.gc(); //Garbage collector para tirar os mediaplayer já mortos
+        pathMusic = Musicas.get(PosTocando).getPathMusic();//Puxa o pathmusic da musica a ser tocada
+        media = new Media(pathMusic); //Carrega a Media
+        mp = new MediaPlayer(media);
+        mp.setOnEndOfMedia(new CheckNextMusic()); //Chama a próxima musica ao terminar a atual
+    }
+
+    //Metodo para iniciar os listener dos sliders
+    private void startListenerSlider() {
+        //Prepara o Slider
+        mp.getMedia().durationProperty().addListener(listener -> setSeekerSlider());
+        //Mantem o slider seguindo a música
+        mp.currentTimeProperty().addListener(teste);
+    }
+
+    //Metodo para adicionar o listener para verificar alterações nos metadados
+    private void addListenerMetadata() {
+        Mp3Buf.getInstance().getMp().getMedia().getMetadata().addListener(
+                (MapChangeListener.Change<? extends String, ? extends Object> change) -> {
+                    ControleUI.getInstance().getPlayerController().setInfos(
+                            Mp3Buf.getInstance().getMedia().getMetadata());
+                });
+    }
 
     public void carregaMusica() {//Classe para carregar a música no Media e no MediaPlayer
         if (PosTocando >= Musicas.size()) { //Caso chegue na ultima música retorna ao inicio
@@ -142,58 +138,14 @@ public final class Mp3Buf {
         if (PosTocando < 0) { //Caso o usuário esteja na ultima música e aperta o anterior vai para a  ultima da lista
             PosTocando = Musicas.size() - 1;
         }
-        mp = null;
-        media = null;
-        System.gc();
-        pathMusic = Musicas.get(PosTocando).getPathMusic();//Puxa o pathmusic da musica a ser tocada
-        media = new Media(pathMusic); //Carrega a Media
-        mp = new MediaPlayer(media);
-        mp.setOnEndOfMedia(new CheckNextMusic());
-        //Metodo para verificar as alterações nos metadados
-        Mp3Buf.getInstance().getMp().getMedia().getMetadata().addListener((MapChangeListener.Change<? extends String, ? extends Object> change) -> {
-            ControleUI.getInstance().getPlayerController().setInfos(
-                    Mp3Buf.getInstance().getMedia().getMetadata());
-        });
-        //Prepara o Slider
-        mp.getMedia().durationProperty().addListener(listener -> setSeekerSlider());
-        //Mantem o slider seguindo a música
-        mp.currentTimeProperty().addListener(teste);
-        mp.getAudioEqualizer().getBands().clear();
-        double mid = (EqualizerBand.MAX_GAIN - EqualizerBand.MIN_GAIN) / 2;
-        double freq = 20;
-        for (int i = 0; i <= 9; i++) {
-            Mp3Buf.getInstance().getMp().getAudioEqualizer().getBands().add(new EqualizerBand(freq, freq / 2, 0));
-            freq *= 2;
-        }
-        //ControleUI.getInstance().getPlayerController().setInfos(media.getMetadata());
-        if (bandas != null) { //Verifica se o array já foi inicializado
-            for (int i = 0; i < bandas.size(); i++) {// Carrega os dados no array
-
-                mp.getAudioEqualizer().getBands().get(i).setGain(bandas.get(i).getValor());
-            }
-        }
+        preparaMp();
+        addListenerMetadata();
+        preparaEq();
+        startListenerSlider();
 
     }
-    private double lastseekValue;
 
-    public double getLastseekValue() {
-        return lastseekValue;
-    }
-
-    public void setLastseekValue(double lastseekValue) {
-        this.lastseekValue = lastseekValue;
-    }
-    
-    private double valorTrackerAnterior;
-
-    public double getValorTrackerAnterior() {
-        return valorTrackerAnterior;
-    }
-
-    public void setValorTrackerAnterior(double valorTrackerAnterior) {
-        this.valorTrackerAnterior = valorTrackerAnterior;
-    }
-    
+    //Códigos para controle dos trackers
     private void setTrackerSliderPos() {
         ControleUI.getInstance().getPlayerController().getTracker().setValue(mp.getCurrentTime().toMinutes());
     }
@@ -212,4 +164,77 @@ public final class Mp3Buf {
         //System.out.println(ControleUI.getInstance().getPlayerController().getTracker().getValue());
         mp.seek(Duration.minutes(time));
     }
+
+    //Getter e setter das váriaveis
+    public double getLastseekValue() {
+        return lastseekValue;
+    }
+
+    public void setLastseekValue(double lastseekValue) {
+        this.lastseekValue = lastseekValue;
+    }
+
+    public double getValorTrackerAnterior() {
+        return valorTrackerAnterior;
+    }
+
+    public void setValorTrackerAnterior(double valorTrackerAnterior) {
+        this.valorTrackerAnterior = valorTrackerAnterior;
+    }
+
+    public ArrayList<Musica> getMusicas() {
+        return Musicas;
+    }
+
+    public void setMusicas(ArrayList<Musica> Musicas) {
+        this.Musicas = Musicas;
+    }
+
+    public String getPathMusic() {
+        return pathMusic;
+    }
+
+    public void setPathMusic(String pathMusic) {
+        this.pathMusic = pathMusic;
+    }
+
+    public Media getMedia() {
+        return media;
+    }
+
+    public void setMedia(Media media) {
+        this.media = media;
+    }
+
+    public MediaPlayer checkmp() {
+        return mp;
+    }
+
+    public MediaPlayer getMp() {
+        if (mp == null) {
+            carregaMusica();
+        }
+        return mp;
+    }
+
+    public void setMp(MediaPlayer mp) {
+        this.mp = mp;
+    }
+
+    public int getPosTocando() {
+        return PosTocando;
+    }
+
+    public void setPosTocando(int PosTocando) {
+        this.PosTocando = PosTocando;
+    }
+
+    public ArrayList<Banda> getBandas() {
+        return bandas;
+    }
+
+    public void setBandas(ArrayList<Banda> bandas) {
+        this.bandas = bandas;
+    }
+
 }
